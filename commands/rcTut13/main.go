3a6b3c731cc0ad147c888b398e4852ebc82ff662
Pradeep Mahdevu <pradeep@clearplan.io>
2016-04-25 08:40:47 -0700

    Simple POST example

--- commands/rcTut13/main.go
+++ commands/rcTut13/main.go
@@ -6,20 +6,25 @@ import (
 
 	"github.com/zenazn/goji"
 	"github.com/zenazn/goji/web"
+	"encoding/json"
+	"github.com/spotlmnop/goLangIntro/models"
 )
 
 func hello(c web.C, w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello, %s!", c.URLParams["name"])
 }
 
-
-func createUser(c web.C, w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, %s!", c.URLParams["name"])
+func createUser(c web.C, w http.ResponseWriter, req *http.Request) {
+	decoder := json.NewDecoder(req.Body)
+	var u models.User
+	decoder.Decode(&u)
+	w.Header().Set("Content-Type", "application/json")
+	encoder := json.NewEncoder(w)
+	encoder.Encode(u)
 }
-
 
 func main() {
 	goji.Get("/hello/:name", hello)
-	goji.Post("/user/", createUser)
+	goji.Post("/user", createUser)
 	goji.Serve()
 }
